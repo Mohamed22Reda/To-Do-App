dc22d1c8a235a4d293d0b65c8388bae6fe574ff8
Mohamed Reda <godamohamad3@gmail.com>
2023-11-12 14:52:01 +0200

    create structure

---
+++ lib/core/utls/app_assets.dart
@@ -0,0 +1,11 @@
+class AppAssets
+{
+  static const String basePath='assets/images/';
+  static const String logo='${basePath}UpToDo.png';
+  static const String on1='${basePath}On1.png';
+  static const String on2='${basePath}On2.png';
+  static const String on3='${basePath}On3.png';
+  static const String noTasks='${basePath}NoTasks.png';
+
+
+}
---
+++ lib/core/utls/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class AppColors
+{
+  static Color primary = Color(0xff8685E7);
+  static Color background = Color(0xffFFFFFF);
+  static Color white = Color(0xff121212);
+  static Color red = Color(0xffFF4666);
+  static Color green = Color(0xff66CC41);
+  static Color blueGrey = Color(0xff41CCA7);
+  static Color blue = Color(0xff4181CC);
+  static Color orange = Color(0xffCC8441);
+  static Color purple = Color(0xff9741CC);
+  static Color grey = Color(0xff979797);
+  static Color deebGrey = Color(0xff424242);
+
+
+
+}
